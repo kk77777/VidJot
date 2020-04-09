0c900de523ea2f3db709f18bf8f35097f1c6ab68
kk77777 <kaushikgattani@gmail.com>
2020-04-09 21:29:46 +0530

    started creating routes

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express(); //Initalise the app
 
+//Home Page Route
+app.get('/', (req, res) => {
+  res.send('Home');
+});
+
+//About Page Route
+app.get('/about', (req, res) => {
+  res.send('About');
+});
+
 const port = 4000;
 
 app.listen(port, () => {
