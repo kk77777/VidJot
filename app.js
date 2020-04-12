940e72423df82c2848eb5f4adb60b1dee49bfc45
kk77777 <kaushikgattani@gmail.com>
2020-04-12 16:24:10 +0530

    debugging+displaying data from database

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars');
+const {
+  allowInsecurePrototypeAccess,
+} = require('@handlebars/allow-prototype-access');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('dotenv').config();
@@ -35,13 +39,17 @@ app.engine(
   'handlebars',
   exphbs({
     defaultLayout: 'main',
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
   })
 );
 app.set('view engine', 'handlebars');
 
 //Home Page Route
 app.get('/', (req, res) => {
-  res.render('home');
+  const title = 'Home';
+  res.render('index', {
+    title: title,
+  });
 });
 
 //About Page Route
@@ -52,6 +60,17 @@ app.get('/about', (req, res) => {
 //Add Idea Form
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add');
+});
+
+//Get Idea data from database
+app.get('/ideas', (req, res) => {
+  Idea.find({})
+    .sort({ date: 'desc' })
+    .then((ideas) => {
+      res.render('ideas/index', {
+        ideas: ideas,
+      });
+    });
 });
 
 //Process Idea Form
